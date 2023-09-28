ce7ebe952adbc946d00955fd23615e8bc9d00c0e
Pablo Daniel Rey <pablo.rey@epicio.com>
2023-09-28 20:14:41 -0300

    added the basic code for a web api. and the basic code for a docker container

--- proyecto1/ecoissue/ecoissue/main.py
+++ proyecto1/ecoissue/ecoissue/main.py
@@ -1,1 +1,13 @@
-print("Hola mundo")
+from fastapi import FastAPI
+
+api = FastAPI()
+
+
+@api.get("/")
+def index():
+    data = {"status": 200, "message": "Hola Mundo"}
+    return data
+
+
+def app():
+    return api
