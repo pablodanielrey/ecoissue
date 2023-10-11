d11b5ae5af3698b9d76c65989e7af010d9558c17
mariapaz <marypazch1@gmail.com>
2023-10-11 10:52:36 -0300

    modificacion programa. while

--- proyecto1/ecoissue/ecoissue/sistema_pedidos.py
+++ proyecto1/ecoissue/ecoissue/sistema_pedidos.py
@@ -1,10 +1,13 @@
 # crea un programa que solicite numero de pedido. si este es = 0, fin.
 #else, almacenar los numeros de pedido en el archivo pedidos.txt
-n_pedido = input("Ingrese el nº de pedido :")
 
-if n_pedido == 0:
-    exit()
+
+while True:
+    n_pedido = input("Ingrese el nº de pedido :")
+    
+    if n_pedido == 0:
+        break
 else:
     archivo = open('pedidos.txt' , 'a')
-    archivo.write(str(n_pedido))
+    archivo.write(str(n_pedido))   
     archivo.close()
