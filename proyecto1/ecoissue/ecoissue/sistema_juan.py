3e4e4bc35066160faf8d018fc3d486feedc04d95
Juan V. Castro <juan.v.castro@gmail.com>
2023-10-12 10:15:23 -0300

    2da consigna

--- proyecto1/ecoissue/ecoissue/sistema_juan.py
+++ proyecto1/ecoissue/ecoissue/sistema_juan.py
@@ -1,6 +1,15 @@
-pedido = input("Ingrese el número de pedido: ")
+while True:
+    pedido = input("Ingrese el número de pedido: ")
 
-cache = open("pedidos_juan.txt", "a")
-cache.write(str(pedido))
-cache.write("\n")
-cache.close()
+    if pedido == "0":
+        break
+    else:
+        cache = open("pedidos_juan.txt", "a")
+        cache.write("pedido: ")
+        cache.write(str(pedido))
+        cache.write("\n")
+        cache.close()
+
+        print("Escribiendo el número:", pedido)
+
+print("Cerrando Sistema...")
