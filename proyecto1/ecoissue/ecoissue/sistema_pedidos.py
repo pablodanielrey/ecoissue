d12fc85a1edd478c05b4bcf0b498a5d95c41e57d
mariapaz <marypazch1@gmail.com>
2023-10-12 09:27:03 -0300

    consigna 2

--- proyecto1/ecoissue/ecoissue/sistema_pedidos.py
+++ proyecto1/ecoissue/ecoissue/sistema_pedidos.py
@@ -1,28 +1,10 @@
-# consigna 1:
-# crea un programa que solicite numero de pedido. si este es = 0, fin.
-# else, almacenar los numeros de pedido en el archivo pedidos.txt
-#
 
-
-# consigna 2:
-# modifique su sistema para que, ademas de escribir en un archivo, ahora imprima
-# por pantalla cada número que se ingreso por teclado. y la leyenda:
-# Escribiendo el número : xxxx
-# adicionalmente el archivo de texto donde se encuentran los números escritos
-# debe tener no solo el número, si no que un texto "pedido".
-# ej:
-# pedido: 1
-# pedido: 45
-# pedido: 23
-# ....
-# Ayuda:
-# puede utilizar la sintaxis f de strings para incluír el valor de las
-# variables dentro del string a imprimir/escribir
-# ej:   f"Esto es un string con la variable {variable}"
-
+#Archivo Maria Paz.
 
 while True:
+
     n_pedido = input(str("Ingrese el nº de pedido :"))
+    print("Escribiendo el numero: ", n_pedido)
 
     if n_pedido == "0":
         break
