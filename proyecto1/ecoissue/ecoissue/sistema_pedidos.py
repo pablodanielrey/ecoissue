c134c1a48175ea887428dac2bc91907895f2d55f
mariapaz <marypazch1@gmail.com>
2023-10-17 11:22:01 -0300

    consigna 4

--- proyecto1/ecoissue/ecoissue/sistema_pedidos.py
+++ proyecto1/ecoissue/ecoissue/sistema_pedidos.py
@@ -1,26 +1,24 @@
-
 #Archivo Maria Paz.
 from datetime import datetime
-
-
+lista = []
 while True:
-    
     n_pedido = input(str("Ingrese el nº de pedido :"))
     fh = datetime.now()
     almacenamiento ={
         'numero' : n_pedido,
         'fecha' : fh
     }
+    
     print("Escribiendo el numero: ", almacenamiento["numero"])
-
+  
     if almacenamiento["numero"] == "0":
         break
     else:
-        lista = []
+       
         lista.append(almacenamiento)
         
 
-archivo = open("/tmp/pedidos.txt", "a")
+archivo = open("/tmp/pedidos.txt", "w")
 for elemento in lista:
     texto = str(f"Pedido {elemento['numero']} fecha y hora {elemento['fecha']}")
     archivo.write(texto)
