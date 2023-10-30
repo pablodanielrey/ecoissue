5b209c6c0fee94b747a11f43d5c9aed8e4ad5724
LucasRomeo18 <lucas.romeo18@gmail.com>
2023-10-30 17:18:08 -0300

    Consigna 7 de Lucas

--- proyecto1/ecoissue/ecoissue/lucas/pedidos7.py
+++ proyecto1/ecoissue/ecoissue/lucas/pedidos7.py
@@ -7,3 +7,40 @@
 # debe modificar tambien el programa que muestra los pedidos a partir del archivo
 # para adecuarlo al nuevo formato de archivo a leer. y debe imprimir cuando se 
 # cargo la información.
+
+
+import datetime
+import json
+fecha_time = datetime.datetime.now()
+hora_time = datetime.datetime.today()
+fecha = fecha_time.strftime("%d,%m,%Y")
+hora = hora_time.strftime("%H,%M")
+###### Diccionario para cuando se ejecunta el programa ########
+info_ejecucion_json = {
+    "Fecha de ejecución: " : fecha,
+    "Hora de ejecución: " : hora,
+    "pedidos_final" : []  #Lista para almacenar los pedidos
+}
+###### Diccionario para cuando se ejecunta el programa ########
+
+with open( "pedidos.json", "a") as archivo:
+    while True:
+        n_pedidos = input("Ingrese número de pedido(0 para terminar el programa): ")
+        if n_pedidos == str(0):
+            print ("Ingreso el número 0, se guardaron los número de pedidos en pedidos.json")                    
+            print ("Programa terminado.")
+            break
+        else:
+             ####### Diccionario de los pedidos #######
+            pedidos = {
+            "número: " : n_pedidos,
+            "Fecha: " : fecha,
+            "Hora: " : hora,
+            }
+            ####### Diccionario de los pedidos #######
+        info_ejecucion_json["pedidos_final"].append(pedidos)
+with open ("pedidos.json", "w") as archivo_json:
+    json.dump(info_ejecucion_json, archivo_json, indent=4)
+    print("Escribiendo los pedidos en el archivo JSON")
+    print("Fecha de ejecución: " , info_ejecucion_json["Fecha de ejecución: "])
+    print("Hora de ejecución: " , info_ejecucion_json["Hora de ejecución: "])
