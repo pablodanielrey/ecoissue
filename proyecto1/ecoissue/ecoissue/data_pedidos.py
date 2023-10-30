4e9bd73824151dec591700fe01a876e2286b8ad2
mariapaz <marypazch1@gmail.com>
2023-10-30 10:25:21 -0300

    consigna 6

--- proyecto1/ecoissue/ecoissue/data_pedidos.py
+++ proyecto1/ecoissue/ecoissue/data_pedidos.py
@@ -6,6 +6,3 @@ with open("/tmp/pedidos.txt") as archivo:
             dato = json.loads(linea) 
              
             print("El pedido es: ", dato["numero"])
-
-#corregir el for para que itere en cada linea del archivo. de las dos pruebas solo toma el primero o el segundo 
-#movi la variable. 
